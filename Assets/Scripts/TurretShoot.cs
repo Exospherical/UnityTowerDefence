757f9d61b7626352f377ae42ae5657e48bc523ca
Exospherical <ryan.woodford56@ed.cna.nl.ca>
2021-03-13 22:25:41 -0330

    General improvements
    - Added floating camera controllable with WASD + mouse
    
    - Added rudimentary turret rotator towards nearest enemy
    
    - Added simple enemy spawning script

--- Assets/Scripts/TurretShoot.cs
+++ Assets/Scripts/TurretShoot.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class TurretShoot : MonoBehaviour
 {
 
+    private float shootTimer = 0.5f;
+    private float shootTimerMax = 0.5f;
+
+    public Transform target;
+    public float range = 15f;
+    public float turnSpeed = 10f;
 
     // Update is called once per frame
 
     public GameObject bulletPrefab;
+    public Transform parttoRotate;
+
+    public string enemyTag = "Enemy";
+
+    void Start()
+    {
+        InvokeRepeating("updateTarget", 0f, 0.5f);
+    }
 
     void Update()
     {
-       
+        if (target == null)
+        {
+            return;
+        }
 
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookrotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(parttoRotate.rotation, lookrotation, Time.deltaTime * turnSpeed) .eulerAngles;
+        parttoRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        shootTimer -= Time.deltaTime;
     }
 
 
@@ -21,14 +44,51 @@ public class TurretShoot : MonoBehaviour
     void OnTriggerStay(Collider co)
 
     {
-
-        // Was it a Monster? Then Shoot it
-        if (co.GetComponent<MonsterScript>())
+        if (shootTimer <= 0f)
         {
-            GameObject g = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            g.GetComponent<Bullet>().target = co.transform;
+            shootTimer = shootTimerMax;
+
+            // Was it a Monster? Then Shoot it
+            if (co.GetComponent<MonsterScript>())
+            {
+            //    GameObject g = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+             //   g.GetComponent<Bullet>().target = co.transform;
+            }
         }
+
+
+
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+
+    void updateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistanceToEnemy = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistanceToEnemy)
+            {
+                shortestDistanceToEnemy = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistanceToEnemy <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else { target = null; }
+
+    }
 
 }
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public int startSpawnTime = 5;
+    public int spawnTime = 5;
+    public GameObject enemy;
+    private int time = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", startSpawnTime, spawnTime);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Spawn()
+    {
+       
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, new Vector3(-14, 0.05f, -13), transform.rotation);
+    }
+}
