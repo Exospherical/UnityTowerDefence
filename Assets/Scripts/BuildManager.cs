10060412a8ce2e26e8ea74744061bf314d443e27
Exospherical <ryan.woodford56@ed.cna.nl.ca>
2021-03-14 19:59:00 -0230

    Added turret building on specific prefab
    - Enabled onhover for turretPlatform prefabs (to a red color) to show where turrets can be built.
    
    -The turret platforms can be clicked on to spawn a turret on top of the (OnClick)
    
    -Started adding shop UI. 3 buttons on interface, with a picture for the 1 completed turret as the sprite for the first button.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public static BuildManager instance;
+    private GameObject turretToBuild;
+    public GameObject standardTurretPrefab;
+
+    public GameObject getTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
