db678073f203012545aa869aeadedef0c2c5660b
Exospherical <ryan.woodford56@ed.cna.nl.ca>
2021-03-15 22:37:10 -0230

    Rebuilt turret building functionality with adding currency in the future
    added a points static class to track player points
    
    rebuilt turret building to easily add turret costs in the future

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    public TurretFactory standardTurret;
+    public TurretFactory secondTurret; 
+    BuildManager buildManager;
+
+    private void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+    public void selectStandardTurret()
+    {
+        buildManager.selectTurretToBuild(standardTurret);
+    }
+
+    public void selectUpgradedTurret()
+    {
+        buildManager.selectTurretToBuild(secondTurret);
+
+    }
+}
